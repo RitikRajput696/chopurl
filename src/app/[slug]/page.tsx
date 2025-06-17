a96b84f171fdc4aacd1b9c89a18a1dd1f815dd0f
RitikRajput696 <rixusinput696@gmail.com>
2025-06-17 21:32:50 +0530

    fixed slug error finally

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,20 +4,23 @@ import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 interface SlugPageProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
 export default async function RedirectPage(props: SlugPageProps) {
   const params = await props.params;
-  const slug = await params.slug;
+  const slug = params.slug;
+
   console.log(slug);
+
   const result = await db.select().from(urls).where(eq(urls.slug, slug));
 
   if (result.length === 0) {
     return <div className="p-8 text-center">URL not found</div>;
   }
+
   console.log(result);
   const originalUrl = result[0].originalUrl;
   console.log(originalUrl);
